71cf60896c6584b8b1d8c196cceb777cf79518c8
mukil krishna B <112685725+mukil2@users.noreply.github.com>
2022-10-21 20:45:24 +0530

    Add files via upload

---
+++ testclass2.java
@@ -0,0 +1,19 @@
+ public class testclass2 
+{
+ int a=5;
+testclass3 obj=new testclass3();
+ public void methodname1() 
+   {
+obj.methodname2();
+ System.out.println("hello11:"+a);
+   }
+}
+
+ class testclass3
+{
+ int a=5;
+ public void methodname2() 
+   {
+    System.out.println("hello2:"+a);
+   }
+}
